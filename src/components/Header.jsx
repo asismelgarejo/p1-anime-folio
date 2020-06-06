c226d8dbf83664765cd6d11f4982dcc7d63e0736
Asís <asismelgarejo@gmail.com>
2020-06-05 21:33:59 -0500

    Configuración completa

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <p>Hola</p>
+  );
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import '../assets/style.scss';
+
+ReactDOM.render(<Header />, document.getElementById('container'));
